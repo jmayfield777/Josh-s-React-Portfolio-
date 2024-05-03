5e9b38f12cdcfe5fa63b55ac06c9b30f0bd630f6
Josh <josh.mayfield45@gmail.com>
2024-05-03 15:52:06 -0400

    updated ResumePage.module.css styling and added import statement to ResumePage.jsx

--- react-portfolio-2/src/pages/ResumePage.jsx
+++ react-portfolio-2/src/pages/ResumePage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
+import styles from "./ResumePage.module.css";
+
 export default function Resume() {
   return (
-    <section>
+    <section className={styles.container}>
       <div>
-        <h1>Resume</h1>
+        <h1 className={styles.title}>Resume</h1>
       </div>
       <div>
         <a href={require("../assets/Resume/Joshua_Mayfield_Resume_OLD.pdf")} download>
-          <h4>Download my resume here!</h4>
+          <h4 className={styles.resumeTitle}>Download my resume here!</h4>
         </a>
       </div>
     </section>
