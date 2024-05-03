359b1368d7158333a918209cbdedb01f63345d97
Josh <josh.mayfield45@gmail.com>
2024-05-02 22:20:26 -0400

    updated main.jsx

--- react-portfolio-2/src/main.jsx
+++ react-portfolio-2/src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+// imports 
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
+import ErrorPage from './pages/ErrorPage.jsx';
+import Profile from './pages/ProfilePage.jsx';
+import About from './pages/AboutPage.jsx';
+import Projects from './pages/ProjectsPage.jsx';
+import Resume from './pages/ResumePage.jsx';
+
 import './index.css'
 
+import "@fontsource/roboto";
+
+// define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Profile />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Projects',
+        element: <Projects />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
